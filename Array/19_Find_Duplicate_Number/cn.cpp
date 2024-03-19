fcb0d5fef38bad6af88829a8cf186527ea6f7ebe
Harsh Jethva <141603654+jethvaharsh333@users.noreply.github.com>
2024-03-19 20:15:40 +0530

    Create cn.cpp

---
+++ Array/19_Find_Duplicate_Number/cn.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+  int freq[n + 1] = {0};
+
+  for (int i = 0; i < n; i++) {
+    if (freq[arr[i]] == 0) {
+      freq[arr[i]] += 1;
+    } 
+    else {
+      return arr[i];
+    }
+  }
+
+  return 0;
+}
