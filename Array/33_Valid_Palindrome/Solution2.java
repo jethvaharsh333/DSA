0a2d1200cb39c74d8057ff0cb709e57f5cae6bd6
Harsh Jethva <141603654+jethvaharsh333@users.noreply.github.com>
2024-10-31 16:42:02 +0530

    Update Solution2.java

--- Array/33_Valid_Palindrome/Solution2.java
+++ Array/33_Valid_Palindrome/Solution2.java
@@ -1,24 +1,43 @@
-// O(nlogn), 100% beats
+// O(n), 100% beats
 class Solution2 {
-    public int[] twoSum(int[] numbers, int target) {
-      int left = 0;
-      int right = numbers.length - 1;
-      int curSum;
-      while(left <= right){
-        curSum = numbers[left] + numbers[right];
-        if(curSum == target){
-            return new int[]{left+1, right+1};
-        }
-       if(curSum < target){
-                int index = Arrays.binarySearch(numbers, left + 1, right -1, target-numbers[right]);
-                index = index > 0 ? index : Math.abs(index) - 1;
-                left = index > left ? index : left + 1;
-            }else{
-                int index = Arrays.binarySearch(numbers, left + 1, right -1, target - numbers[left]);
-                index = index > 0 ? index : Math.abs(index) - 1;
-                right = index < right ? index : right - 1;
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 1)
+            return true;
+        
+        int start = 0, end = s.length() - 1;
+
+        while (start < end) {
+            char sCur = s.charAt(start);
+            char eCur = s.charAt(end);
+            
+            // converting to lower case
+            if (sCur >= 'A' && sCur <= 'Z')
+                sCur = (char) (sCur + 32);
+            if (eCur >= 'A' && eCur <= 'Z')
+                eCur = (char) (eCur + 32);
+
+            // validating for lower characters and numbers
+            boolean sValid = (sCur >= 'a' && sCur <= 'z') || (sCur >= '0' && sCur <= '9');
+            boolean eValid = (eCur >= 'a' && eCur <= 'z') || (eCur >= '0' && eCur <= '9');
+            
+            if (!sValid) {
+                start++;
+                continue;
             }
-      }
-      return null;
+            if (!eValid) {
+                end--;
+                continue;
+            }
+
+            if (sValid && eValid) {
+                if (sCur == eCur) {
+                    start++;
+                    end--;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
