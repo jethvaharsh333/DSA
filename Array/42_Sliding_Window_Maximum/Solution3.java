6e02055d60936785626fb2523694e92f31065675
Harsh Jethva <141603654+jethvaharsh333@users.noreply.github.com>
2024-11-05 12:42:56 +0530

    Update Solution3.java

--- Array/42_Sliding_Window_Maximum/Solution3.java
+++ Array/42_Sliding_Window_Maximum/Solution3.java
@@ -1,3 +1,4 @@
+// Time complexity: O(n^2); Space Complexity: O(1); OPTIMAL SOLUTION
 class Solution3 {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int[] re = new int[nums.length+1-k];
