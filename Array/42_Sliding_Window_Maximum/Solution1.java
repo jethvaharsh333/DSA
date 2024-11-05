2b932f845189ce1af71021e01876db24d8bacb60
Harsh Jethva <141603654+jethvaharsh333@users.noreply.github.com>
2024-11-05 12:28:26 +0530

    Create Solution1.java

---
+++ Array/42_Sliding_Window_Maximum/Solution1.java
@@ -0,0 +1,16 @@
+class Solution1 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n-k+1];
+
+        for (int i=0 ; i<=n-k ; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j=i ; j<i+k ; j++) {
+                max = Math.max(max, nums[j]);
+            }
+            result[i] = max;
+        }
+
+        return result;
+    }
+}
