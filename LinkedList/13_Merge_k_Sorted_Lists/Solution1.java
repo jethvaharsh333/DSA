901f48ab14cd4b7ac150de9d09ebc4ba32604ffa
Harsh Jethva <141603654+jethvaharsh333@users.noreply.github.com>
2024-11-17 07:19:16 +0530

    Create Solution1.java

---
+++ LinkedList/13_Merge_k_Sorted_Lists/Solution1.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution1 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> allNodes = new ArrayList<>();
+
+        for (ListNode node : lists) {
+            while (node != null) {
+                allNodes.add(node.val);
+                node = node.next;
+            }
+        }
+
+        Collections.sort(allNodes);
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        for (int val : allNodes) {
+            current.next = new ListNode(val);
+            current = current.next;
+        }
+
+        return dummy.next;
+    }
+}
