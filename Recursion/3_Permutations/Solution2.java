84ad5d828fa4f2860adc1bb1b8418770e24b6b5a
Harsh Jethva <141603654+jethvaharsh333@users.noreply.github.com>
2024-11-19 10:39:42 +0530

    Update Solution2.java

--- Recursion/3_Permutations/Solution2.java
+++ Recursion/3_Permutations/Solution2.java
@@ -1,46 +1,31 @@
-// 0ms, 44.3MB
-
+// Time Complexity: O(n!); Space Complexity: O(n)
 class Solution2 {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(nums, 0, result);
+        genR(nums, 0, result);
         return result;
     }
 
-    private void backtrack(int[] nums, int start, List<List<Integer>> result) {
-        if (start == nums.length) {
-            List<Integer> permutation = new ArrayList<>();
-            for (int num : nums) {
-                permutation.add(num);
+    private void genR(int[] nums, int begin, List<List<Integer>> result){
+        if(begin == nums.length-1){
+            List<Integer> li = new ArrayList<>();
+            for(int x: nums){
+                li.add(x);
             }
-            result.add(permutation);
+            result.add(li);
             return;
         }
-
-        for (int i = start; i < nums.length; i++) {
-            swap(nums, start, i);         // Swap current element with the start
-            backtrack(nums, start + 1, result); // Recurse with the next start index
-            swap(nums, start, i);         // Backtrack (undo the swap)
+        
+        for(int i=begin ; i<nums.length ; i++){
+            swap(nums, begin, i);
+            genR(nums, begin+1, result);
+            swap(nums, begin, i);       // 2nd swap => backtrack
         }
     }
 
-    private void swap(int[] nums, int i, int j) {
+    private void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 }
-
-/*
-                              [1, 2, 3]   
-                                   |
-                      -----------------------------------
-                      |               |                |
-               [1, 2, 3]         [2, 1, 3]        [3, 2, 1]   
-                 |                 |                |
-           -------------     --------------     -------------
-           |           |     |            |     |           |
-        [1, 2, 3]  [1, 3, 2] [2, 1, 3]  [2, 3, 1] [3, 2, 1] [3, 1, 2]
-           |           |     |            |     |           |
-        [1, 2, 3]  [1, 3, 2] [2, 1, 3]  [2, 3, 1] [3, 2, 1] [3, 1, 2]   (Final permutations)
-*/
