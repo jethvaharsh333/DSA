defd2899560c2a8d760030a1af979b993e62d000
Harsh Jethva <141603654+jethvaharsh333@users.noreply.github.com>
2024-11-22 17:27:37 +0530

    Create Solution.java

---
+++ Recursion/9_Letter_Combinatn_of_Phone_No/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    private static String[] dict = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+
+        StringBuilder str = new StringBuilder();
+        genCombination(digits, str, result);
+
+        return result;
+    }
+
+    private void genCombination(String digits, StringBuilder str, List<String> result){
+        if(digits.length() == 0){
+            result.add(str.toString());
+            return;
+        }
+    
+        for(char ch: dict[digits.charAt(0) - '0'].toCharArray()){
+            str.append(ch);
+            genCombination(digits.substring(1), str, result);
+            str.deleteCharAt(str.length()-1);
+        }
+    }
+}
