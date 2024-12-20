b88d22b532be79bccb092f222eb5dfd1ac5220e8
Harsh Jethva <141603654+jethvaharsh333@users.noreply.github.com>
2024-12-20 11:07:05 +0530

    Update Solution2.java

--- Graph/04_Max_Area_of_Island/Solution2.java
+++ Graph/04_Max_Area_of_Island/Solution2.java
@@ -1,4 +1,6 @@
-class Solution {
+// Time Complexity: O(m*n), Space Complexity: O(min(m*n)) 
+
+class Solution2 {
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
