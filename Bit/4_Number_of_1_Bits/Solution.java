626f50639aca5a3558e9ce4df6f22c35324f2069
Harsh Jethva <141603654+jethvaharsh333@users.noreply.github.com>
2025-01-30 22:17:10 +0530

    Create Solution.java

---
+++ Bit/4_Number_of_1_Bits/Solution.java
@@ -0,0 +1,17 @@
+// Time Complexity: O(logn), Space Complexity: O(1)
+
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        
+        while(n > 0){
+            if((n&1) == 1){
+                count++;
+            }
+
+            n >>= 1;
+        }
+
+        return count;
+    }
+}
