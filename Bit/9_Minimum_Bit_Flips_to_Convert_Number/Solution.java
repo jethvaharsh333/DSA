7a082f683a34dafc85cac2fe75aa8495314a8a8c
Harsh Jethva <141603654+jethvaharsh333@users.noreply.github.com>
2025-02-04 20:05:02 +0530

    Update Solution.java

--- Bit/9_Minimum_Bit_Flips_to_Convert_Number/Solution.java
+++ Bit/9_Minimum_Bit_Flips_to_Convert_Number/Solution.java
@@ -1,19 +1,15 @@
 class Solution {
-    public int[] plusOne(int[] digits) {
-        int size = digits.length;
+    public int minBitFlips(int start, int goal) {
+        int num = start^goal;
+        int count = 0;
 
-        for(int i=size-1 ; i>=0 ; i--){
-            if(digits[i] < 9){
-                digits[i]++;
-                return digits;
+        while(num>0){
+            if((num & 1) == 1){
+                count++;
             }
-            
-            digits[i] = 0;
+            num >>= 1;
         }
 
-        int[] newNumber = new int[size+1];
-        newNumber[0] = 1;
-        
-        return newNumber;
+        return count;
     }
 }
