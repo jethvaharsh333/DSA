d7f94e83ee638ee5f1f95ff57fc7c43a4325df5d
jethvaharsh333 <jethvaharsh333@gmail.com>
2025-05-29 19:41:37 +0530

    sw-1

---
+++ All_Patterns/src/Sliding_Window/AA_SS_sizeThree_DistChar.java
@@ -0,0 +1,49 @@
+package Sliding_Window;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters
+Time complexity: O(n), Space complexity: O(1)
+* */
+
+public class AA_SS_sizeThree_DistChar {
+    static int[] arr = new int[26];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        Arrays.fill(arr, 0);
+        int count = 0, duplicateCount = 0;
+
+        if (s.length() < 3){
+            System.out.println("Count: " + count);
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            char curr = s.charAt(i);
+            arr[curr - 'a']++;
+            if (arr[curr - 'a'] == 2) duplicateCount++;
+        }
+
+        if (duplicateCount == 0) count++;
+
+        for (int i = 3; i < s.length(); i++) {
+            char outChar = s.charAt(i - 3);
+            char inChar = s.charAt(i);
+
+            arr[outChar - 'a']--;
+            if (arr[outChar - 'a'] == 1) duplicateCount--;
+
+            arr[inChar - 'a']++;
+            if (arr[inChar - 'a'] == 2) duplicateCount++;
+
+            if (duplicateCount == 0) count++;
+        }
+
+        System.out.println("Count: " + count);
+    }
+}
