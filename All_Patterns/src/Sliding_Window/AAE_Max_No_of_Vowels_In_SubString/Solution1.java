07274fb9c3dcfcba6447cbd486000afbd4d55bbe
jethvaharsh333 <jethvaharsh333@gmail.com>
2025-06-03 08:50:13 +0530

    1 sw questions with 2 approached solved 3/6/25 added

---
+++ All_Patterns/src/Sliding_Window/AAE_Max_No_of_Vowels_In_SubString/Solution1.java
@@ -0,0 +1,62 @@
+package Sliding_Window.AAE_Max_No_of_Vowels_In_SubString;
+
+import java.util.Scanner;
+
+/*
+* https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+* */
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter String: ");
+        String s = sc.nextLine();
+
+        System.out.println("Enter k: ");
+        int k = sc.nextInt();
+
+        int ans = maxVowels(s, k);
+        System.out.println(ans);
+    }
+
+    public static int maxVowels(String s, int k){
+        int len = s.length();
+        StringBuilder sb = new StringBuilder(s.substring(0,k));
+        int maxCount = 0;
+        int consecutiveCount = 0;
+
+        for(int i=0; i<k ; i++){
+            if(isVowel(sb.charAt(i))){
+                consecutiveCount++;
+            }
+        }
+
+        maxCount = consecutiveCount;
+
+        for(int i=k; i<len ; i++){
+            if(isVowel(sb.charAt(0))){
+                consecutiveCount--;
+            }
+
+            sb.deleteCharAt(0);
+
+            char ch = s.charAt(i);
+            sb.append(ch);
+            if(isVowel(ch)){
+                consecutiveCount++;
+            }
+
+            maxCount = Math.max(maxCount, consecutiveCount);
+        }
+
+        return maxCount;
+    }
+
+    public static boolean isVowel(char ch){
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ All_Patterns/src/Sliding_Window/AAE_Max_No_of_Vowels_In_SubString/Solution2.java
@@ -0,0 +1,41 @@
+package Sliding_Window.AAE_Max_No_of_Vowels_In_SubString;
+
+import java.util.Scanner;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter String: ");
+        String s = sc.nextLine();
+
+        System.out.println("Enter k: ");
+        int k = sc.nextInt();
+
+        int ans = maxVowels(s, k);
+        System.out.println("MaxCount: " + ans);
+    }
+
+    public static int maxVowels(String s, int k) {
+        int[] arr = new int[26];
+        arr['a'-'a'] = 1;
+        arr['e'-'a'] = 1;
+        arr['i'-'a'] = 1;
+        arr['o'-'a'] = 1;
+        arr['u'-'a'] = 1;
+
+        int maxVowCount=0, currVowCount=0;
+
+        for(int i=0 ; i<s.length() ; i++){
+            currVowCount += arr[s.charAt(i)-'a'];
+
+            if(i>=k){
+                currVowCount -= arr[s.charAt(i-k)-'a'];
+            }
+
+            maxVowCount = Math.max(maxVowCount, currVowCount);
+        }
+
+        return maxVowCount;
+    }
+}
